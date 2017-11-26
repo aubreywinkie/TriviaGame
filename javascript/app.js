29b62f6a5109ef90ffca74325dc03d50afb058f6
Aubrey Winkie <aubreywinkie@utexas.edu>
2017-11-26 10:40:29 -0600

    working timer

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,33 @@
 
+$("#startButton").click(function() {
+  $("#splashScreen").hide();
+  $("gameCanvas").show();
+});
+
+var totalSeconds = 60*0.5;
+var minutes = parseInt(totalSeconds/60);
+var seconds = parseInt(totalSeconds%60);
+
+function timer() {
+  $("#quiz-time-left").text('Time Left: ' + minutes + ' minutes ' + seconds + ' seconds');
+  if(totalSeconds <=0) {
+    setTimeout(function(){ alert("Times Up!"); }, 1);
+    // setTimeout('document.quiz.submit()', 1);
+  } else{
+    totalSeconds = totalSeconds - 1;
+    minutes = parseInt(totalSeconds/60);
+    seconds = parseInt(totalSeconds%60);
+  setTimeout("timer()", 1000);
+  }
+};
+
+$("#startButton").click(function() {
+  timer();
+});
+
+// setTimeout("timer()", 1000);
+
+
   $(".test").on('click', function() {
     if($(this).val() == 'ans') {
       alert("Correct Answer!")
@@ -7,3 +36,4 @@
       
     }
     });
+
